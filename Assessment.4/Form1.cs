809358565a4193e7565dee395a4ccb4f19a1f6ce
matthewguillory65 <matthew.guillor@ad.aie.edu>
2017-02-24 17:16:39 -0600

    Fixes and work to the assessment dealing with making the comboboxes the hero in the attack and defend functions

--- Assessment.4/Form1.cs
+++ Assessment.4/Form1.cs
@@ -16,28 +16,29 @@ namespace Assessment._4
         public Form1()
         {
             InitializeComponent();
-            
+
             //comboBox1.Items.Add(new Heros(10, 200, "Defender"));
             //comboBox1.Items.Add(new Heros(160, 140, "Archer"));
             //comboBox1.Items.Add(new Heros(180, 120, "Warrior"));
             //comboBox1.Items.Add(SI.Heroz[0].Name);
             //comboBox2.Items.Add(SI.Heroz[0].Name);
-            Singleton.Instance.Heroz.ForEach(x => {
+            Singleton.Instance.Heroz.ForEach(x =>
+            {
                 comboBox1.Items.Add(x.Name);
                 comboBox2.Items.Add(x.Name);
             });
             textBox2.Text = SI.currentDrogoon.Attack.ToString();
             textBox3.Text = SI.currentDrogoon.Alive.ToString();
             textBox1.Text = SI.currentDrogoon.Name;
-            if(!Singleton.Instance.currentDrogoon.Alive)
+            if (!Singleton.Instance.currentDrogoon.Alive)
             {
                 Singleton.Instance.Combat.IfDead().ToString();
             }
-            SI.currentHero = 
-            ;
-            
-            Singleton.Instance.Combat.Attack(comboBox1, comboBox2, Singleton.Instance.currentDrogoon);
-            Singleton.Instance.Combat.Defend(comboBox1, comboBox2, Singleton.Instance.currentDrogoon);
+
+
+
+            //Singleton.Instance.Combat.Attack(comboBox1, comboBox2, Singleton.Instance.currentDrogoon);
+            //Singleton.Instance.Combat.Defend(comboBox1, comboBox2, Singleton.Instance.currentDrogoon);
 
             //comboBox2.Items.Add(new Heros(10, 200, "Defender"));
             //comboBox2.Items.Add(new Heros(160, 140, "Archer"));
@@ -46,27 +47,27 @@ namespace Assessment._4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
